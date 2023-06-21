322912877c428215ab1cd67dea090ddd0c7d7823
Chris <timurnyaa@gmail.com>
2023-06-21 14:48:25 +0300

    dont work

---
+++ Assets/_main/Scripts/Core/IO/FileManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class FileManager
+{
+    public static List<string> ReadTextFiles(string FilePath, bool includeBlacnkLines = true)
+    {
+        if (!filePath.StartsWith('/'))
+            filePath = FilePath.root + filePath;
+        
+        List<string> lines = new List<string>();
+        try 
+        {
+            using(StreamReader sr = new StreamReader(filePath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    if (includeBlacnkLines || !string.IsNullOrWhiteSpace(line))
+                        lines.Add(line);
+                }
+            }
+        }
+        catch (FileNotFoundException ex)
+        {
+            
+        }
+        return lines;
+    }
+
+    public static List<string> ReadTextAsset(string FilePath, bool includeBlacnkLines = true)
+    {
+        return null; 
+    }
+}
---
+++ Assets/_main/Scripts/Core/IO/FilePaths.cs
@@ -0,0 +1,4 @@
+public class FilePaths
+{
+    public static readonly string root = $"{Application.dataPath}/gameData/";
+}
