85694f08ffc02129b6cc491ea6f4db6f70f106d9
Chris <timurnyaa@gmail.com>
2023-06-21 15:58:30 +0300

    string to TextAsset

--- Assets/testing/TextFiles.cs
+++ Assets/testing/TextFiles.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TextFiles : MonoBehaviour
 {
-    private string fileName = "testingFiles"; 
+    [SerializeField] private TextAsset fileName; 
     // Start is called before the first frame update
     void Start()
     {
